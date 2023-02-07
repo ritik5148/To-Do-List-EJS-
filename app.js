91c9979417011e0cf470e1d3cc012be9ce782bc0
Ritik Kumar <rk0602541@gmail.com>
2023-02-07 19:09:36 +0530

    Completed Project

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const BodyParser = require("body-parser");
+
+const app = express();
+app.set("view engine","ejs");
+app.use(BodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+var items=["Buy Food","Cook Food","Eat Food"];
+
+app.get("/",function(req,res){
+    var today = new Date();
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month:"long"
+    }
+    var day = today.toLocaleDateString("en-US",options);
+    res.render("list",{kindOfDay:day,newListItems:items}); 
+})
+
+app.post("/",function(req,res)
+{
+    var item = req.body.newItem;
+    items.push(item);
+    
+    res.redirect("/");
+
+})
+
+app.listen(3000,function()
+{
+    console.log("Server is running on port 3000");
+})
